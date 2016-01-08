8f93353a6f7614eaa443293ef7a68a60ff97fd36
Susilvignesh <susil95@gmail.com>
2016-01-08 13:29:47 +0530

    Create position.py
    In this file the position of the board is returned.

---
+++ position.py
@@ -0,0 +1,75 @@
+
+def positionnum(position):
+
+	if position == 1:
+
+		return 0 ,0
+
+	elif position == 2:
+
+		return 0, 1
+
+	elif position == 3:
+
+		return 0, 2
+
+	elif position == 4:
+
+		return 1, 0
+
+	elif position == 5:
+
+		return 1, 1
+
+	elif position == 6:
+
+		return 1, 2
+
+	elif position == 7:
+
+		return 2, 0
+
+	elif position == 8:
+
+		return 2, 1
+
+	elif position == 9:
+
+		return 2, 2
+	
+	elif position == [0, 0]:
+		
+		return 1
+	
+	elif position == [0, 1]:
+		
+		return 2
+	
+	elif position == [0, 2]:
+		
+		return 3
+	
+	elif position == [1, 0]:
+		
+		return 4
+		
+	elif position == [1, 1]:
+		
+		return 5
+		
+	elif position == [1, 2]:
+		
+		return 6
+	
+	elif position == [2, 0]:
+		
+		return 7
+		
+	elif position == [2, 1]:
+		
+		return 8
+		
+	elif position == [2, 2]:
+		
+		return 9
+
